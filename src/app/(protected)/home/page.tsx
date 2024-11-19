6760eb0ae9559494a83b1a2f443407a5dd2dfe9c
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-20 04:25:13 +0800

    todo: homepage

---
+++ src/app/(protected)/home/page.tsx
@@ -0,0 +1,10 @@
+import { LogoutButton } from "@/components/buttons/LogoutButton";
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <LogoutButton>Logout</LogoutButton>
+      </div>
+    );
+  }
