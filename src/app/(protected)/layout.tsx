69855b90bb7f28558b89c6094a35dea094f91ab8
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-21 03:40:50 +0800

    header and footer for protected pages layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,6 +2,9 @@ import db from "@/lib/db";
 import { validateRequest } from "@/auth";
 import { SessionProvider } from "@/lib/auth/SessionContext";
 import { redirect } from "next/navigation";
+import { PageContainer } from "@/components/containers/PageContainer";
+import Header from "@/components/partials/Header";
+import { Footer } from "@/components/partials/Footer";
 
 export default async function ProtectedLayout({
   children,
@@ -33,7 +36,13 @@ export default async function ProtectedLayout({
   
   return (
     <SessionProvider value={session}>
-      <div className="flex flex-col min-h-dvh">{children}</div>
+      <div className="flex flex-col">
+        <Header />
+        <PageContainer>
+          {children}
+        </PageContainer>
+        <Footer />
+      </div>
     </SessionProvider>
   );
 }
