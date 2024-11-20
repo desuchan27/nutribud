8e2ba77c3d3d5953f3ffa589faf5ac80075dc258
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-21 03:40:58 +0800

    wip: profile page

---
+++ src/app/(protected)/[profile]/page.tsx
@@ -0,0 +1,10 @@
+// import { getUser } from "@/actions/onboarding.actions";
+
+export default async function Profile() {
+
+    return (
+      <div>
+        <h1>Profile</h1>
+      </div>
+    );
+  }
