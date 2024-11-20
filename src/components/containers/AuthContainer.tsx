5eed4b294970466755fe7c8d4bf1313098b1108d
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-21 02:21:55 +0800

    renaming auth to container folder

--- src/components/containers/AuthContainer.tsx
+++ src/components/containers/AuthContainer.tsx
@@ -9,7 +9,7 @@ export const AuthContainer = ({
         <div className="w-full md:w-1/2 h-full bg-gray-50">
             <div className="max-w-xl h-full flex flex-col gap-y-8 px-4 py-4 md:px-8 md:py-8 items-center justify-center mx-auto">
                 <div className="relative w-[252px] h-[88px]">
-                    <Image src="/icons/nutribud-icon.png" fill alt="NutriBud Logo" className='cover'/>
+                    <Image src="/icons/nutribud-icon.svg" fill alt="NutriBud Logo" className='cover'/>
                 </div>
                 {children}
             </div>
