66a59343989b6471e2408e4602664f685835061f
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-21 03:39:35 +0800

    global user type for reusable purposed

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface UserProps {
+  id: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  userName: string | undefined;
+  email: string | undefined;
+  profileImage?: string | undefined | null;
+}
