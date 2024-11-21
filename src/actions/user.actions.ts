86bdb4e2e74a60cac929912e2e03ac4fca7accda
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-21 23:50:53 +0800

    submit user value function

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,9 +1,36 @@
+"use server"
+
+import { validateRequest } from "@/auth"
 import db from "@/lib/db"
 
 export const getUserInfo = (id: string) => {
     const user = db.user.findFirst({
         where: {
             id: id,
+        },
+    })
+
+    return (user)
+}
+
+export const submitUserBio = async (id: string, bio: string) => {
+
+    const session = await validateRequest();
+
+    if (session.user?.id === undefined) {
+        throw new Error("User ID is required")
+    }
+
+    if (session.user?.id !== id) {
+        throw new Error("User ID does not match the session user ID")
+    }
+
+    const user = db.user.update({
+        where: {
+            id: id,
+        },
+        data: {
+            bio: bio,
         },
     })
 
