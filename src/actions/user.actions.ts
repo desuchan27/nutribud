35da6b20d15f7c6cbf0d94740fbd87255a3088ee
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-22 02:55:12 +0800

    submit profile picture functionality

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -1,38 +1,78 @@
-"use server"
+"use server";
 
-import { validateRequest } from "@/auth"
-import db from "@/lib/db"
+import { validateRequest } from "@/auth";
+import db from "@/lib/db";
 
 export const getUserInfo = (id: string) => {
-    const user = db.user.findFirst({
-        where: {
-            id: id,
-        },
-    })
+  const user = db.user.findFirst({
+    where: {
+      id: id,
+    },
+  });
 
-    return (user)
-}
+  return user;
+};
 
 export const submitUserBio = async (id: string, bio: string) => {
+  const session = await validateRequest();
 
+  if (session.user?.id === undefined) {
+    throw new Error("User ID is required");
+  }
+
+  if (session.user?.id !== id) {
+    throw new Error("User ID does not match the session user ID");
+  }
+
+  const user = db.user.update({
+    where: {
+      id: id,
+    },
+    data: {
+      bio: bio,
+    },
+  });
+
+  return user;
+};
+
+export const uploadProfileImage = async (id: string, image:string
+) => {
+
+  console.log("Received imageUrl:", image); // Log received imageUrl
+
+  try {
     const session = await validateRequest();
+    const sessionId = session.user?.id;
 
-    if (session.user?.id === undefined) {
-        throw new Error("User ID is required")
-    }
+    const existingUser = await db.user.findFirst({
+      where: {
+        id: sessionId,
+      },
+    });
 
-    if (session.user?.id !== id) {
-        throw new Error("User ID does not match the session user ID")
-    }
-
-    const user = db.user.update({
+    if (!existingUser) {
+      return {
+        error: "Unauthorized",
+      };
+    } else {
+      await db.user.update({
         where: {
-            id: id,
+          id: existingUser.id,
         },
         data: {
-            bio: bio,
+          profileImage: image,
         },
-    })
-
-    return (user)
-}
+      });
+      console.log("Profile updated successfully");
+      return {
+        success: "Profile updated successfully",
+      };
+    }
+  } catch (error) {
+    console.error("Error updating avatar:", error); // Log any errors
+    return {
+      error: "An error occurred while updating the profile image",
+    };
+  }
+};
