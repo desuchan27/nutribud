575e787ad41f272daa7f3fd4325adf0ab1ea5d11
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-25 06:18:58 +0800

    user settings base page with data fetching

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,7 +1,29 @@
+import { validateRequest } from "@/auth";
+import { PageContainer } from "@/components/containers/PageContainer";
+import { UserInfoSettingsForm, UserSettingsForm } from "@/components/forms/UserSettingsForm";
+import db from "@/lib/db";
+
 export default async function Settings() {
+  const session = await validateRequest();
+
+  const getUserData = await db.user.findUnique({
+    where: {
+      id: session.user?.id,
+    },
+  });
+
+  const getUserInfoData = await db.userInfo.findUnique({
+    where: {
+      userId: session.user?.id,
+    },
+  });
+
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <PageContainer>
+      <div className="flex flex-col md:flex-row">
+        <UserSettingsForm userData={getUserData} />
+        <UserInfoSettingsForm userInfoData={getUserInfoData} />
+      </div>
+    </PageContainer>
   );
 }
