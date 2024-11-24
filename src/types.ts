8b066c128c767a5f3f36c9f6cd252d36bb2e41d6
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-25 03:26:45 +0800

    experimental: reusable interfaces

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,22 @@ export interface UserProps {
   email: string | undefined;
   profileImage?: string | undefined | null;
 }
+
+// export interface RecipeImage {
+//   id: string;
+//   img: string;
+//   recipeId: string;
+// }
+
+// export interface Recipe {
+//   id: string;
+//   title: string;
+//   ingredients: string;
+//   procedure: string;
+//   createdAt: string; // Change to string
+//   recipeImage: RecipeImage[];
+//   user: {
+//     username: string;
+//     image: string;
+//   };
+// }
