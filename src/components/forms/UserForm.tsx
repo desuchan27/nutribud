3e475a83e7e71b394b0e8be2808bb1b61280826e
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-25 02:08:57 +0800

    user form

--- src/components/forms/UserForm.tsx
+++ src/components/forms/UserForm.tsx
@@ -2,14 +2,22 @@
 
 import * as z from "zod";
 import Image from "next/image";
-import { userBioSchema, userProfileImageSchema } from "@/schema";
+import {
+  userBioSchema,
+  userProfileImageSchema,
+  userRecipeSchema,
+} from "@/schema";
 import { FaCamera, FaEdit, FaPlus } from "react-icons/fa";
 import { useModal } from "@/components/UseModal"; // Adjust the import path as needed
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { startTransition, useState } from "react";
 import { useSession } from "@/lib/auth/SessionContext";
-import { submitUserBio, uploadProfileImage } from "@/actions/user.actions";
+import {
+  submitUserBio,
+  submitUserRecipe,
+  uploadProfileImage,
+} from "@/actions/user.actions";
 import toast from "react-hot-toast";
 import { UploadButton } from "@/utils/uploadthing";
 
@@ -21,6 +29,18 @@ interface UserBioFormProps {
 interface UserProfileFormProps {
   image?: string;
   onProfileUpdate: (newProfile: string) => void;
+}
+
+// interface RecipeFormProps {
+//   id: string;
+//   title: string;
+//   ingredients: string;
+//   procedure: string;
+//   image: string[];
+// }
+
+interface UploadResponse {
+  url: string;
 }
 
 export function UserBioForm({ bio, onBioUpdate }: UserBioFormProps) {
@@ -81,12 +101,14 @@ export function UserBioForm({ bio, onBioUpdate }: UserBioFormProps) {
       </button>
 
       {modal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 px-4 md:px-0 z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 px-4 md:px-0 z-50 text-zinc-700">
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="w-full h-3/4 md:w-1/2 md:max-h-1/2 md:h-fit flex flex-col bg-gray-100 p-4 gap-4 rounded-tr-xl rounded-bl-[3rem]"
           >
-            <h2>{bioHeader}</h2>
+            <h2 className="font-semibold text-lg text-zinc-600 mb-2">
+              {bioHeader}
+            </h2>
             <textarea
               {...form.register("bio")}
               className="w-full p-2 border rounded-md h-full"
@@ -186,14 +208,15 @@ export function UserProfileForm({
       </button>
 
       {modal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 px-4 md:px-0 z-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 px-4 md:px-0 z-50 text-zinc-700">
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="w-full h-3/4 md:w-1/2 md:max-h-1/2 md:h-fit flex flex-col justify-between bg-gray-100 p-4 gap-4 rounded-tr-xl rounded-bl-[3rem]"
+            className="w-full h-3/4 md:w-1/2 md:max-h-1/2 md:h-fit flex flex-col justify-between bg-gray-100 p-4 gap-5 rounded-tr-xl rounded-bl-[3rem] overflow-auto"
           >
-            <h2>Add Profile</h2>
+            <h2 className="font-semibold text-lg text-zinc-600 mb-2">
+              Add Profile
+            </h2>
             <div className="flex flex-col items-center justify-center">
-              
               {uploadedFileName && (
                 <div className="mt-4 w-full flex flex-col items-center justify-center space-y-2">
                   <div className="h-full w-full flex items-center justify-center">
@@ -211,7 +234,7 @@ export function UserProfileForm({
                 </div>
               )}
               <UploadButton
-              className="mt-4 ut-button:bg-primary hover:ut-button:bg-primary/75 ut-button:ut-readying:bg-primary/50"
+                className="mt-4 ut-button:bg-primary hover:ut-button:bg-primary/75 ut-button:ut-readying:bg-primary/50"
                 // appearance={{
                 //   button({ ready, isUploading }) {
                 //     return `custom-button ${
@@ -242,7 +265,10 @@ export function UserProfileForm({
               />
             </div>
             <div className="flex justify-end mt-4">
-              <button onClick={close} className="px-4 py-2 bg-primary text-white rounded-md hover:bg-primary/75 transition-bg duration-200">
+              <button
+                onClick={close}
+                className="px-4 py-2 bg-primary text-white rounded-md hover:bg-primary/75 transition-bg duration-200"
+              >
                 Done
               </button>
             </div>
@@ -253,8 +279,61 @@ export function UserProfileForm({
   );
 }
 
-export function UserPostForm() {
+export function UserRecipeForm() {
+//   {
+//   id,
+//   title,
+//   ingredients,
+//   procedure,
+//   image,
+// }: RecipeFormProps
   const { modal, handleOpenModal, handleCloseModal } = useModal();
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+
+  // const form = useForm<z.infer<typeof userRecipeSchema>>({
+  //   resolver: zodResolver(userRecipeSchema),
+  //   defaultValues: {
+  //     title: title || "",
+  //     ingredients: ingredients || "",
+  //     procedure: procedure || "",
+  //     image: image || [],
+  //   },
+  // });
+
+  const form = useForm<z.infer<typeof userRecipeSchema>>({
+    resolver: zodResolver(userRecipeSchema),
+    defaultValues: {
+      title: "",
+      ingredients: "",
+      procedure: "",
+      image: [],
+    },
+  });
+
+  // const session = useSession();
+  // const userId = session?.user?.id;
+
+  const onSubmit = async (values: z.infer<typeof userRecipeSchema>) => {
+    try {
+      await submitUserRecipe({ ...values, image: uploadedImages });
+      toast.success("Recipe posted successfully!");
+      handleCloseModal();
+    } catch (error) {
+      console.error("Error submitting recipe:", error);
+      toast.error("An unknown error occurred");
+    }
+  };
+
+  const handleUploadComplete = (res: UploadResponse[]) => {
+    if (res && res.length > 0) {
+      const uploadedUrls = res.map((file) => file.url);
+      setUploadedImages((prev) => [...prev, ...uploadedUrls]);
+      form.setValue("image", [...uploadedImages, ...uploadedUrls]);
+      alert("Upload Completed");
+    } else {
+      alert("No files uploaded.");
+    }
+  };
 
   return (
     <>
@@ -262,18 +341,73 @@ export function UserPostForm() {
         className="flex flex-row items-center justify-center gap-2 px-2 py-2 md:px-4 md:py-2 rounded-lg bg-primary text-white text-sm md:text-base"
         onClick={handleOpenModal}
       >
-        <FaPlus /> Add Post
+        <FaPlus /> Upload Recipe
       </button>
 
       {modal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-          <div className="bg-gray-100 p-4 rounded-lg h-1/2 w-1/2">
-            <h2>Add Post</h2>
-            <textarea
-              className="w-full p-2 border rounded-md"
-              rows={4}
-              placeholder="Enter your post..."
-            ></textarea>
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50 px-4 md:px-0 z-50 text-zinc-700">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full h-3/4 md:w-1/2 md:max-h-1/2 md:h-fit flex flex-col justify-between bg-gray-100 p-4 gap-5 rounded-tr-xl rounded-bl-[3rem] overflow-auto"
+          >
+            <h2 className="font-semibold text-lg text-zinc-600 mb-2">
+              Share Your Recipe
+            </h2>
+
+            <UploadButton
+              className="mt-4 ut-button:bg-primary hover:ut-button:bg-primary/75 ut-button:ut-readying:bg-primary/50"
+              endpoint="imageUploader"
+              onClientUploadComplete={handleUploadComplete}
+              onUploadError={(error: Error) => {
+                alert(`ERROR! ${error.message}`);
+              }}
+            />
+
+            <div className="flex flex-row gap-2 overflow-x-auto">
+              {uploadedImages.map((url, index) => (
+                <div
+                  key={index}
+                  className="relative w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]"
+                >
+                  <Image
+                    src={url}
+                    alt={`Uploaded ${index}`}
+                    fill
+                    className="object-cover rounded-md"
+                  />
+                </div>
+              ))}
+            </div>
+
+            <span className="flex flex-col gap-2">
+              <label className="text-sm text-zinc-700">Recipe Title</label>
+              <input
+                {...form.register("title")}
+                className="w-full p-2 border rounded-md"
+                placeholder="No Meat Vegetarian Burger (100% Guaranteed)"
+              />
+            </span>
+
+            <span className="flex flex-col gap-2">
+              <label className="text-sm text-zinc-700">Ingredients</label>
+              <textarea
+                {...form.register("ingredients")}
+                className="w-full p-2 border rounded-md h-full"
+                rows={4}
+                placeholder="List your ingredients..."
+              />
+            </span>
+
+            <span className="flex flex-col gap-2">
+              <label className="text-sm text-zinc-700">Procedures</label>
+              <textarea
+                {...form.register("procedure")}
+                className="w-full p-2 border rounded-md h-full"
+                rows={4}
+                placeholder="List your procedures..."
+              />
+            </span>
+
             <div className="flex justify-end mt-4">
               <button
                 onClick={handleCloseModal}
@@ -281,11 +415,14 @@ export function UserPostForm() {
               >
                 Cancel
               </button>
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
-                Save
+              <button
+                type="submit"
+                className="px-4 py-2 bg-primary text-white rounded-md hover:bg-primary/75 transition-bg duration-200"
+              >
+                Publish Recipe
               </button>
             </div>
-          </div>
+          </form>
         </div>
       )}
     </>
