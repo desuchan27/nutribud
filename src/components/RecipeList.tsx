1de62733eccbff73903cd0ba968b4135c20d3fee
Desu JS <122036484+desuchan27@users.noreply.github.com>
2024-11-25 02:08:01 +0800

    recipe list component for homepage and profile

---
+++ src/components/RecipeList.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+// import { PageContainer, PageContainerGray } from "@/components/containers/PageContainer";
+import RecipeCard from "@/components/RecipeCard";
+
+interface RecipeListProps {
+  recipes: {
+    id: string;
+    title: string;
+    ingredients: string;
+    procedure: string;
+    recipeImage: { id: string; img: string }[];
+    user: {
+      username: string;
+        image: string;
+    };
+  }[];
+}
+
+export default function RecipeList({ recipes }: RecipeListProps) {
+  return (
+    <div className="bg-gray-100">
+      <div className="min-h-dvh max-w-4xl flex flex-col mx-auto px-4 py-8 gap-6">
+          {recipes.map((recipe) => (
+            <RecipeCard key={recipe.id} recipe={recipe} />
+          ))}
+      </div>
+    </div>
+  );
+}
