2af4c4f54027801ac6f5ac2e85a8c759f4022fba
Desu Chan <orenodesukouichi@gmail.com>
2024-11-28 22:42:07 +0800

    finalizing recipe detail style

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { PageContainer } from "./containers/PageContainer";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { CommentSection, ReactionSection } from "./CommentSection";
 
 interface RecipeDetailProps {
   recipe: {
@@ -39,8 +40,8 @@ export default function RecipeDetail({ recipe }: RecipeDetailProps) {
 
   return (
     <PageContainer>
-      <div className="flex flex-col gap-5 px-5 py-5 max-w-7xl mx-auto">
-        <div className="flex flex-row justify-between">
+      <div className="flex flex-col gap-5 px-5 py-5 w-full max-w-7xl mx-auto">
+        <div className="flex flex-col gap-4">
           <h1 className="text-lg md:text-2xl font-semibold">{recipe.title}</h1>
           <Link
             href={`/${recipe.user.username}`}
@@ -58,51 +59,65 @@ export default function RecipeDetail({ recipe }: RecipeDetailProps) {
             </p>
           </Link>
         </div>
-        <div className="relative w-full aspect-square sm:aspect-video">
-          <Image
-            src={recipe.recipeImage[currentImageIndex].img}
-            alt={recipe.title}
-            fill
-            className="rounded-lg object-cover"
-          />
-          {moreThanOneImage && (
-            <>
-              <button
-                onClick={handlePrevImage}
-                className="absolute left-2 top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white w-10 h-10 flex items-center justify-center rounded-full"
-              >
-                <FaChevronLeft />
-              </button>
-              <button
-                onClick={handleNextImage}
-                className="absolute right-2 top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white w-10 h-10 flex items-center justify-center rounded-full"
-              >
-                <FaChevronRight />
-              </button>
-            </>
-          )}
-          <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-1">
-            {recipe.recipeImage.map((_, index) => (
-              <div
-                key={index}
-                className={`w-2 h-2 rounded-full ${
-                  index === currentImageIndex ? "bg-white" : "bg-gray-400"
-                }`}
-              ></div>
-            ))}
+        <div className="w-full flex flex-col md:flex-row gap-10">
+          <div className="flex flex-col gap-5 w-full md:w-3/4">
+            <div className="relative w-full aspect-square sm:aspect-video">
+              <Image
+                src={recipe.recipeImage[currentImageIndex].img}
+                alt={recipe.title}
+                fill
+                className="rounded-lg object-cover"
+              />
+              {moreThanOneImage && (
+                <>
+                  <button
+                    onClick={handlePrevImage}
+                    className="absolute left-2 top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white w-10 h-10 flex items-center justify-center rounded-full"
+                  >
+                    <FaChevronLeft />
+                  </button>
+                  <button
+                    onClick={handleNextImage}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 bg-black bg-opacity-50 text-white w-10 h-10 flex items-center justify-center rounded-full"
+                  >
+                    <FaChevronRight />
+                  </button>
+                </>
+              )}
+              <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex gap-1">
+                {recipe.recipeImage.map((_, index) => (
+                  <div
+                    key={index}
+                    className={`w-2 h-2 rounded-full ${index === currentImageIndex ? "bg-white" : "bg-gray-400"
+                      }`}
+                  ></div>
+                ))}
+              </div>
+            </div>
+            <ReactionSection />
+            <div className="hidden md:flex w-full md:w-fit flex-col gap-2">
+              <h2 className="text-base md:text-lg font-semibold">Procedures</h2>
+              <p className="whitespace-pre-wrap">{recipe.procedure}</p>
+            </div>
+            <CommentSection />
+          </div>
+          <div className="flex flex-col md:flex-row gap-10 h-full w-full md:w-1/4 text-sm md:text-base">
+            <div className="hidden md:flex flex-col gap-2 w-fit">
+              <h2 className="text-base md:text-lg font-semibold">Ingredients</h2>
+              <p className="whitespace-pre-wrap">{recipe.ingredients}</p>
+            </div>
+            {/* <div className="border-r border-2 border-gray-200 h-full" /> */}
+            <div className="md:hidden flex flex-col gap-2 w-fit">
+              <h2 className="text-base md:text-lg font-semibold">Ingredients</h2>
+              <p className="whitespace-pre-wrap">{recipe.ingredients}</p>
+            </div>
+            <div className="md:hidden flex w-full md:w-fit flex-col gap-2">
+              <h2 className="text-base md:text-lg font-semibold">Procedures</h2>
+              <p className="whitespace-pre-wrap">{recipe.procedure}</p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row gap-10 h-full w-full text-sm md:text-base">
-          <div className="md:hidden flex flex-col gap-2">
-            <h2 className="text-base md:text-lg font-semibold">Ingredients</h2>
-            <p className="whitespace-pre-wrap">{recipe.ingredients}</p>
-          </div>
-          {/* <div className="border-r border-2 border-gray-200 h-full" /> */}
-          <div className="w-full md:w-fit flex flex-col gap-2">
-            <h2 className="text-base md:text-lg font-semibold">Procedures</h2>
-            <p className="whitespace-pre-wrap">{recipe.procedure}</p>
-          </div>
-        </div>
+
       </div>
     </PageContainer>
   );
